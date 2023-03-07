14fb31610dda567cec39c9905b216aa8e2dc68fc
Lucas Machado <lukaasmachado143@gmail.com>
2023-03-07 11:09:07 -0300

    Seção 1 - REACT - Inserção de Prioridade, com icones correspondentes, e título

--- pro-atividade-app/src/App.js
+++ pro-atividade-app/src/App.js
@@ -3,18 +3,7 @@ import { useState } from 'react';
 import './App.css';
 
 
-let initiaState = [
-  {
-    id: 1,
-    descricao: "Primeira Atividade"
-  },
-  {
-  id: 2,
-  descricao: "Segunda Atividade"
-  }
-];
-  
-
+let initiaState = [];
 
 function App() {
   const [atividades, setAtividades] = useState(initiaState)
@@ -23,26 +12,64 @@ function App() {
     e.preventDefault();
     const atividade = {
           id: document.getElementById('id').value,
-          descricao: document.getElementById('descricao').value
+          descricao: document.getElementById('descricao').value,
+          prioridade: document.getElementById('inputPrioridade').value,
+          titulo: document.getElementById('titulo').value
       }
     setAtividades([ ...atividades, { ...atividade}]);
   }
 
+  function PrioridadeLabel(aux){
+    switch(aux){
+       case "1": return "Baixa";
+       case "2": return "Média";
+       case "3": return "Alta";
+       default: return "Não Definida"
+    }
+  }
+
+  function PrioridadeIcon(aux){
+    switch(aux){
+       case "1": return "smile";
+       case "2": return "meh";
+       case "3": return "frown";
+       default: return "Não Definida"
+    }
+  }
+
   return (
     <>
-      <form class="row g-3">
-        <div class="col-md-6">
-          <label class="form-label">ID:</label>
-          <input type="text" class="form-control" id="id" />
+      <form className="row g-3">
+        <div className="col-md-6">
+          <label className="form-label">ID:</label>
+          <input type="text" className="form-control" id="id" />
         </div>
-        <div class="col-md-6">
-          <label class="form-label">Descrição:</label>
-          <input type="text" class="form-control" id="descricao" />
+
+        <div className="col-md-6">
+          <label className="form-label">Título:</label>
+          <input type="text" className="form-control" id="titulo" />
         </div>
+
+        <div className="col-md-6">
+          <label className="form-label">Prioridade</label>
+          <select id="inputPrioridade" className="form-select">
+            <option defaultValue="0">Selecionar...</option>
+            <option value="1">Baixa</option>
+            <option value="2">Média</option>
+            <option value="3">Alta</option>
+          
+          </select>
+        </div>
+
+        <div className="col-md-6">
+          <label className="form-label">Descrição:</label>
+          <input type="text" className="form-control" id="descricao" />
+        </div>
+
         <hr />
-        <div class="col-12">
+        <div className="col-12">
           <button 
-            class="btn btn-outline-secondary"
+            className="btn btn-outline-secondary"
             onClick={AddAtividade}
           >
             Adicionar Atividade
@@ -57,13 +84,13 @@ function App() {
               <div className='d-flex justify-content-between'>
                 <h5 className='card-title'>
                   <span className="badge bg-secondary me-2">{ativ.id}</span>
-                   - Título
+                   - {ativ.titulo}
                 </h5>
                 <h6>
                   Prioridade:
                   <span className='ms-1 text-black'>
-                    <i class="me-1 fa-regular fa-face-meh"></i>
-                    Normal   
+                    <i className={"me-1 fa-regular fa-face-"+PrioridadeIcon(ativ.prioridade)}></i>
+                    {PrioridadeLabel(ativ.prioridade)}
                   </span>
                 </h6>
 
